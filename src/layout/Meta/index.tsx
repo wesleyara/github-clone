8ce0681bb1d74566b977f402ef1a30b83939541e
wesleyara <wes1gom@gmail.com>
2022-08-06 16:50:24 -0300

    feat: adicionando tipo de font

--- src/layout/Meta/index.tsx
+++ src/layout/Meta/index.tsx
@@ -27,7 +27,7 @@ const Meta = (props: IMetaProps) => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;700&display=swap"
           rel="stylesheet"
         />
         <link
